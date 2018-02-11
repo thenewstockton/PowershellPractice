72ec991c3859e9c1770dff6661647be5d7885f87
thenewstockton <phillip5914619@gmail.com>
2018-02-11 12:51:39 +0800

    stuff

---
+++ FirstScript.ps1
@@ -0,0 +1,9 @@
+﻿echo Hello
+$PSVersionTable
+
+gci
+start-sleep 2
+cls
+start-sleep 1 
+get-process
+
---
+++ Powershell/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Powershell
+{
+    public class Messages
+    {
+		public bool IsKo1830T { get; set; }
+		public bool IsOnDuty { get; set; }
+
+		public Messages()
+		{
+			IsKo1830T = true; IsOnDuty = true;
+		}
+
+		public void SendMsg()
+		{
+			System.Diagnostics.Process.Start("MSG", "Ko1830T" + " " + "Hello from powershell");
+			MessageBox.Show("IsKo1830T = " + IsKo1830T.ToString());
+		}
+    }
+}
